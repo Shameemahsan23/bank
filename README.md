4512e033ca10540cb1e4f17a71ac7a3e28bed219
shameemahsan23 <shameemahsan80@gmail.com>
2022-01-09 03:13:23 +0600

    add bank

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # bank
-# bank
-# bank
+<!-- echo "# phpecommerce" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Shameemahsan23/bank.git
+git push -u origin main -->
---
+++ script.js
@@ -0,0 +1,16 @@
+// //deposit button evenHendelar
+               
+// const dipositBtn=document.getElementById("addDeposit").addEventListener("click", function (){
+//     const dipositAmount=document.getElementById("depositAmount").value;
+//     const depositNumber = parseFloat(dipositAmount);
+  
+//     const currentDeposit = document.getElementById("currentDeposit"). innerText;
+//     const currentDepositNumber = parseFloat (currentDeposit);
+//     const totalDeposit = depositNumber + currentDepositNumber ;
+    
+//     document.getElementById("currentDeposit"). innerText = totalDeposit;
+
+//       const currentBalance = document.getElementById("currentBalance"). innerText;
+//       const currentBalanceNumber = parseFloat (currentBalance) ; 
+//       const totalBalance = depositNumber + currentBalanceNumber; 
+//       document.getElementById("currentBalance"). innerText = totalBalance;
